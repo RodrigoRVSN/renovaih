1fb26ef6ff2adc75460d36de73355c400b72e9b8
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-01-09 16:50:20 -0300

    ♻ chore: api_local in vercel

--- src/core/services/api.ts
+++ src/core/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:3000/api'
+  baseURL: process.env.NEXT_PUBLIC_API_LOCAL || 'http://localhost:3000/api'
 })
 
 export default api
