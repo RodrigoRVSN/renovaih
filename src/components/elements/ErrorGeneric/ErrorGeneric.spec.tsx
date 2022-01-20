d49b5cf9aff0c68114f8dd1ce5b2355f2cf78115
rodrigorvsn <rodrigovictor81@gmail.com>
2022-01-19 22:21:18 -0300

    🧪 test: error generic and files organization

---
+++ src/components/elements/ErrorGeneric/ErrorGeneric.spec.tsx
@@ -0,0 +1,16 @@
+import { render, fireEvent, screen } from '@testing-library/react'
+import { ErrorGeneric } from '.'
+
+describe('<ErrorGeneric />', () => {
+  it('Should render correctly Generic Error', () => {
+    const fn = jest.fn()
+
+    render(<ErrorGeneric title='Request error' refetch={fn} />)
+
+    fireEvent.click(screen.getByRole('button'))
+    fireEvent.click(screen.getByRole('button'))
+
+    expect(fn).toBeCalledTimes(2)
+    expect(screen.getByText(/Request error/i)).toBeInTheDocument()
+  })
+})
