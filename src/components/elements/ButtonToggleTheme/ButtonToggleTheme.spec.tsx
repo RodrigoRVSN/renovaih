4ce269db3641a6c5dce9572c7b663695e0e9c415
rodrigorvsn <rodrigovictor81@gmail.com>
2023-01-10 23:24:40 -0300

    test: cover ButtonToggleTheme

--- src/components/elements/ButtonToggleTheme/ButtonToggleTheme.spec.tsx
+++ src/components/elements/ButtonToggleTheme/ButtonToggleTheme.spec.tsx
@@ -1,10 +1,39 @@
-import { screen, render } from '@testing-library/react'
+import { screen, render, fireEvent } from '@testing-library/react'
+import { useTheme } from 'next-themes'
 import { ButtonToggleTheme } from '.'
 
+jest.mock('next-themes')
+
+const mockUseTheme = useTheme as jest.MockedFunction<typeof useTheme>
+
+const setThemeMock = jest.fn()
+
 describe('<ButtonToggleTheme />', () => {
-  it('Should render light theme correctly', () => {
+  it('should be able to toggle from light to dark', () => {
+    mockUseTheme.mockImplementation(() => ({
+      theme: 'light',
+      setTheme: setThemeMock,
+      themes: []
+    }))
+
     render(<ButtonToggleTheme />)
 
-    expect(screen.getByText(/claro/i)).toBeInTheDocument()
+    fireEvent.click(screen.getByText(/claro/i))
+
+    expect(setThemeMock).toHaveBeenCalledWith('dark')
+  })
+
+  it('should be able to toggle from dark to light', () => {
+    mockUseTheme.mockImplementation(() => ({
+      theme: 'dark',
+      setTheme: setThemeMock,
+      themes: []
+    }))
+
+    render(<ButtonToggleTheme />)
+
+    fireEvent.click(screen.getByText(/escuro/i))
+
+    expect(setThemeMock).toHaveBeenCalledWith('light')
   })
 })
--- src/components/elements/ButtonToggleTheme/index.tsx
+++ src/components/elements/ButtonToggleTheme/index.tsx
@@ -1,16 +1,18 @@
 import { useTheme } from 'next-themes'
 
-export function ButtonToggleTheme(): JSX.Element {
+export function ButtonToggleTheme() {
   const { theme, setTheme } = useTheme()
+
+  const handleToggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
 
   return (
     <button
       aria-label='Toggle Dark Mode'
       type='button'
-      className='bg-dark_contrast p-lg sm:p-md rounded-full h-lg flex items-center text-text_contrast w-scre'
-      onClick={() => {
-        setTheme(theme === 'dark' ? 'light' : 'dark')
-      }}
+      className='bg-dark_contrast p-lg sm:p-md rounded-full h-lg flex items-center text-text_contrast'
+      onClick={handleToggleTheme}
     >
       {theme === 'dark' ? 'Escuro 🌞' : 'Claro 🌑'}
     </button>
